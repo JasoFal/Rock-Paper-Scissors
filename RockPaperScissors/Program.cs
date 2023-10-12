e8c36f287df3e22aa8d5c28ba9f42ebf838c6a40
Jason Falk <jasonfalk9@gmail.com>
2023-10-12 11:54:10 -0700

    Add rock paper scissors UI logic.

---
+++ RockPaperScissors/Program.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using RockPaperScissors.Models;
+
+namespace RockPaperScissors
+{
+  class Program
+  {
+    static void Main()
+    {
+      Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
+      Console.WriteLine("Welcome to Rock Paper Scissors");
+      Console.WriteLine("Do you wish to play? If so enter 'yes' below.");
+      string userAgreement = Console.ReadLine();
+      if (userAgreement.ToLower() == "yes")
+      {
+        StartNewGame();
+      }
+    }
+
+    static void StartNewGame()
+    {
+      Console.Write("Player 1 enter either rock, paper, or scissors: ");
+      string rpsInput1 = null;
+      while (true)
+      {
+        var key = Console.ReadKey(true);
+        if (key.Key == ConsoleKey.Enter)
+          break;
+        rpsInput1 += key.KeyChar;
+      }
+      Console.Write("Player 2 enter either rock, paper, or scissors: ");
+      string rpsInput2 = null;
+      while (true)
+      {
+        var key = Console.ReadKey(true);
+        if (key.Key == ConsoleKey.Enter)
+          break;
+        rpsInput2 += key.KeyChar;
+      }
+      try
+      {
+        NewRPSGame newGame = new NewRPSGame(rpsInput1, rpsInput2);
+        GameResults(newGame);
+      }
+      catch
+      {
+        Console.WriteLine("!!!!!!!!!!!!!!!");
+        Console.WriteLine("Please enter rock, paper, or scissors");
+        Main();
+      }
+    }
+    static void GameResults(NewRPSGame newGame)
+    {
+      Console.WriteLine($"Player 1 picked {newGame.Player1Input}");
+      Console.WriteLine($"Player 2 picked {newGame.Player2Input}");
+      newGame.CheckRPSWinner();
+      Console.WriteLine($"{newGame.RPSWinner}: WINS!!");
+      Console.WriteLine("-------------------------------------------");
+      Console.WriteLine("Would you like to play again.");
+      Console.WriteLine("If no enter 'new' below");
+      string newGameResponse = Console.ReadLine();
+      if (newGameResponse.ToLower() == "new")
+      {
+        Main();
+      }
+      else
+      {
+        Console.WriteLine("Goodbye!");
+      }
+    }
+  }
+}
